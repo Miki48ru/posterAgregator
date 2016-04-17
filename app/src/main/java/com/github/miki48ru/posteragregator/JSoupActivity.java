86fd709514968cd921d687eb96965ec8a8edad34
Miki48RU <miki48ru@gmail.com>
2016-04-17 21:53:10 +0300

    загрузил библиотеку jsoup и пока эксперементирую с ней в jsoupActivity

---
+++ app/src/main/java/com/github/miki48ru/posteragregator/JSoupActivity.java
@@ -0,0 +1,13 @@
+package com.github.miki48ru.posteragregator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JSoupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jsoup);
+    }
+}
